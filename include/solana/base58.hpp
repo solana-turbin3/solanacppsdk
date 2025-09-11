398b581a89dbc556fbb27c6710c08be273bff1de
prianshpate18 <priyansh.180804@gmail.com>
2025-09-12 00:19:51 +0530

    added include/solana/base58.hpp

---
+++ include/solana/base58.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+namespace solana {
+
+class Base58 {
+public:
+    // Encode from vector of bytes
+    static std::string encode(const std::vector<unsigned char>& input);
+
+    // Decode to vector of bytes
+    static std::vector<unsigned char> decode(const std::string& input);
+
+    // String-friendly helpers
+    static std::string encode_str(const std::string& input);
+    static std::string decode_str(const std::string& input);
+
+private:
+    static const char* const ALPHABET;
+    static const signed char ALPHABET_MAP[128];
+};
+
+}
